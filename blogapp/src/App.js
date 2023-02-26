9c67e998dcbb6f1c86fb39330f4416ccb25b1ec9
Neil Shukla <neilpshukla221203@gmail.com>
2023-02-26 19:35:14 +0530

    frontend initial files created

--- blogapp/src/App.js
+++ blogapp/src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import AddBlog from './pages/AddBlog';
+import AllBlogs from './pages/AllBlogs';
+import OneBlog from './pages/OneBlog';
+import Profile from './pages/Profile';
 
 function App() {
   return (
    <>
+   <Router>
+    <Routes>
+      <Route path='/' element={<AllBlogs/>}/>
+      <Route path='/addblog' element={<AddBlog/>}/>
+      <Route path='/oneblog/:id' element={<OneBlog/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+   </Router>
    </>
   );
 }
